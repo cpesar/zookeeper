435a0ffec7c979d4712bfef32f780c85b357b3e4
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-25 14:39:35 -0600

    added routes to serve all html files

--- server.js
+++ server.js
@@ -197,7 +197,25 @@ function validateAnimal(animal) {
       //The only thing this route does is respond with an HTML page to display in the browser
       //Use the path module to ensure that we are sending to the correct location
       //res.sendFile() has all the built in features for serving to an HTML page
+
+//GET ROUTE TO SERVE INDEX.HTML PAGE
 app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//GET ROUTE TO SERVE ANIMALS.HTML
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+//GET ROUTE TO SERVE ZOOKEEPERS.HTML
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+//WILDCARD ROUTE TO HANDLE ROUTES THAT DON'T EXIST
+  //THIS ROUTE ALWAYS GOES LAST!
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
